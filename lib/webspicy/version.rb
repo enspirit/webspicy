45b7f4bc2f2963f26c05c0694969936b29df1218
Bernard Lambeau <blambeau@gmail.com>
2017-07-31 19:52:26 +0200

    Release v0.3.0-rc1.

--- lib/webspicy/version.rb
+++ lib/webspicy/version.rb
@@ -1,8 +1,8 @@
 module Webspicy
   module Version
     MAJOR = 0
-    MINOR = 2
-    TINY  = 3
+    MINOR = 3
+    TINY  = "0-rc1"
   end
   VERSION = "#{Version::MAJOR}.#{Version::MINOR}.#{Version::TINY}"
 end
