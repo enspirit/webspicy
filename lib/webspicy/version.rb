33e51bfff9dff248f94fc4f1a5c0f39f85769ed5
Bernard Lambeau <blambeau@gmail.com>
2017-08-03 09:43:13 +0200

    Bump to 0.5.0-rc0.

--- lib/webspicy/version.rb
+++ lib/webspicy/version.rb
@@ -1,8 +1,8 @@
 module Webspicy
   module Version
     MAJOR = 0
-    MINOR = 4
-    TINY  = 0
+    MINOR = 5
+    TINY  = "0-rc0"
   end
   VERSION = "#{Version::MAJOR}.#{Version::MINOR}.#{Version::TINY}"
 end
