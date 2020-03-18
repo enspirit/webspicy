8a276e029d54f5d260060063ab7289cc548ca793
Bernard Lambeau <blambeau@gmail.com>
2020-03-18 22:05:43 +0100

    Bump to 0.15.0-rc9 and release.

--- lib/webspicy/version.rb
+++ lib/webspicy/version.rb
@@ -2,7 +2,7 @@ module Webspicy
   module Version
     MAJOR = 0
     MINOR = 15
-    TINY  = "0-rc8"
+    TINY  = "0-rc9"
   end
   VERSION = "#{Version::MAJOR}.#{Version::MINOR}.#{Version::TINY}"
 end
