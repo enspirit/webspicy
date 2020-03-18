0aa3b96bf0ffd4c2eae085d2fbb65bbe4551a7f4
Bernard Lambeau <blambeau@gmail.com>
2020-03-18 17:06:41 +0100

    Bump to 0.15.0-rc6 and release.

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM ruby:2.7
 WORKDIR /home/app
 VOLUME /home/app
 
-RUN gem install -v 0.15.0-rc5 webspicy
+RUN gem install -v 0.15.0-rc6 webspicy
 
 CMD webspicy /home/app/config.rb
--- lib/webspicy/version.rb
+++ lib/webspicy/version.rb
@@ -2,7 +2,7 @@ module Webspicy
   module Version
     MAJOR = 0
     MINOR = 15
-    TINY  = "0-rc5"
+    TINY  = "0-rc6"
   end
   VERSION = "#{Version::MAJOR}.#{Version::MINOR}.#{Version::TINY}"
 end
