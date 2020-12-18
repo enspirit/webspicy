5ec22acd2aee2a22c071654ab595b272a51d0e4a
Bernard Lambeau <blambeau@gmail.com>
2020-12-18 14:19:32 +0100

    Add real config.rb by default in jsonapi example.

--- examples/jsonapi/webspicy/config.rb
+++ examples/jsonapi/webspicy/config.rb
@@ -3,6 +3,8 @@ Finitio.stdlib_path(Path.dir/"schema")
 def webspicy_config(&bl)
   Webspicy::Configuration.new(Path.dir) do |c|
     #c.precondition MustBeAuthenticated
+    c.host = "http://127.0.0.1:4567"
+    c.client = Webspicy::Tester::HttpClient
     c.precondition Webspicy::Specification::Precondition::RobustToInvalidInput.new
     bl.call(c) if bl
   end
