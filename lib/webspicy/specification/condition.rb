63259d03452ce0f7192f1d2dc4db1f96ff7fb2a3
Bernard Lambeau <blambeau@gmail.com>
2021-03-05 14:57:59 +0100

    Move sooner_or_later as a helper in Support module.

--- lib/webspicy/specification/condition.rb
+++ lib/webspicy/specification/condition.rb
@@ -14,17 +14,8 @@ module Webspicy
         end
       end
 
-      SORL_OPTS = { max: 5, wait: 0.05, raise: false }
-
-      def sooner_or_later(opts = nil)
-        opts = SORL_OPTS.merge(opts || {})
-        left, wait_ms = opts[:max], opts[:wait]
-        until (r = yield) || left == 0
-          sleep(wait_ms)
-          wait_ms, left = wait_ms*2, left - 1
-        end
-        raise TimeoutError, "Timeout on sooner-or-later" if r.nil? && opts[:raise]
-        r
+      def sooner_or_later(*args, &bl)
+        Webspicy::Support.sooner_or_later(*args, &bl)
       end
 
     end # module Condition
--- lib/webspicy/support.rb
+++ lib/webspicy/support.rb
@@ -1,3 +1,22 @@
+module Webspicy
+  module Support
+
+    SORL_OPTS = { max: 5, wait: 0.05, raise: false }
+
+    def sooner_or_later(opts = nil)
+      opts = SORL_OPTS.merge(opts || {})
+      left, wait_ms = opts[:max], opts[:wait]
+      until (r = yield) || left == 0
+        sleep(wait_ms)
+        wait_ms, left = wait_ms*2, left - 1
+      end
+      raise TimeoutError, "Timeout on sooner-or-later" if r.nil? && opts[:raise]
+      r
+    end
+    module_function :sooner_or_later
+
+  end # module Support
+end # module Webspicy
 require_relative 'support/data_object'
 require_relative 'support/status_range'
 require_relative 'support/colorize'
