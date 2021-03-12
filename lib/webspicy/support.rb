1f0b44968377b26d7116315f5c6730f57afef221
Bernard Lambeau <blambeau@gmail.com>
2021-03-12 08:09:02 +0100

    Let sooner_or_later block raise Tester::Failure exceptions.

--- lib/webspicy/support.rb
+++ lib/webspicy/support.rb
@@ -3,17 +3,27 @@ module Webspicy
 
     SORL_OPTS = { max: 5, wait: 0.05, raise: false }
 
-    def sooner_or_later(opts = nil)
+    def sooner_or_later(opts = nil, &bl)
       opts = SORL_OPTS.merge(opts || {})
       left, wait_ms = opts[:max], opts[:wait]
-      until (r = yield) || left == 0
+      r = _sooner_or_later(&bl)
+      until (r && !r.is_a?(Tester::Failure)) || left == 0
         sleep(wait_ms)
         wait_ms, left = wait_ms*2, left - 1
+        r = _sooner_or_later(&bl)
       end
+      raise r if r.is_a?(Tester::Failure)
       raise TimeoutError, "Timeout on sooner-or-later" if r.nil? && opts[:raise]
       r
     end
     module_function :sooner_or_later
+
+    def _sooner_or_later(&bl)
+      bl.call
+    rescue Tester::Failure => ex
+      ex
+    end
+    module_function :_sooner_or_later
 
   end # module Support
 end # module Webspicy
--- spec/unit/specification/test_condition.rb
+++ spec/unit/specification/test_condition.rb
@@ -15,6 +15,24 @@ module Webspicy
           expect(x).to eql(nil)
         end
 
+        it 'supports the block raising a Failure' do
+          expect {
+            sooner_or_later(max: 2){
+              raise Tester::Failure
+            }
+          }.to raise_error(Tester::Failure)
+        end
+
+        it 'catches Failure occuring before successes' do
+          seen = 0
+          x = sooner_or_later(max: 2){
+              seen += 1
+            raise Tester::Failure if seen == 1
+            12
+          }
+          expect(x).to eql(12)
+        end
+
         it 'can raise for us' do
           expect{
             sooner_or_later(max: 1, raise: true){ nil }
