a60e68ffc8e5defe0674286aee59a538d8d858e4
Bernard Lambeau <blambeau@gmail.com>
2021-05-01 08:34:07 +0200

    Add failures test in case of wrong host + junit expectations.

--- examples/failures/webspicy/shared_config.rb
+++ examples/failures/webspicy/shared_config.rb
@@ -1,6 +1,23 @@
+class OurOwnClient < Webspicy::Web::RackTestClient
+
+  def initialize(scope)
+    super(scope, ::Sinatra::Application)
+  end
+
+  def _call(test_case)
+    if test_case.specification.url =~ /failing-host/
+      raise "Unable to access host"
+    else
+      super(test_case)
+    end
+  end
+
+end
+
 def webspicy_config(*args, &bl)
   Webspicy::Configuration.new(*args) do |c|
-    c.client = Webspicy::Web::RackTestClient.for(::Sinatra::Application)
+    c.client = OurOwnClient
+
     c.postcondition SucceedingCondition
     c.errcondition SucceedingCondition
     c.postcondition FailingCondition
@@ -9,6 +26,7 @@ def webspicy_config(*args, &bl)
     c.errcondition DeprecatedFailingCondition
     c.postcondition DeprecatedSucceedingCondition
     c.errcondition DeprecatedSucceedingCondition
+
     bl.call(c)
   end
 end
--- examples/failures/webspicy/test.rb
+++ examples/failures/webspicy/test.rb
@@ -4,10 +4,12 @@ webspicy_config(Path.dir) do |c|
   c.world.documentation_output = StringIO.new
   c.world.progress_output = StringIO.new
   c.world.summary_output = StringIO.new
+  c.world.junitxml_output = StringIO.new
   c.colorize = false
   c.reporter = Webspicy::Tester::Reporter::Composite.new
   c.reporter << Tester::Reporter::Documentation.new(c.world.documentation_output)
   c.reporter << Tester::Reporter::Progress.new(c.world.progress_output)
   c.reporter << Tester::Reporter::Summary.new(c.world.summary_output)
-  c.reporter << Tester::Reporter::ErrorCount.new
+  c.reporter << Webspicy::Tester::Reporter::JunitXmlFile.new(c.world.junitxml_output)
+  c.reporter << Tester::Reporter::SuccessOrNot.new
 end
