a652915b9cb27e4e45cf29a35c8e46e0829a0e36
Louis Lambeau <llambeau@quadrabee.com>
2021-06-02 18:34:18 +0200

    WIP

---
+++ lib/webspicy/tester/fakesendgrid/email.rb
@@ -0,0 +1,30 @@
+module Webspicy
+  class Tester
+    class Fakesendgrid
+      class Email
+
+        def initialize(data)
+          @data = data
+        end
+        attr_reader :data
+
+        def from
+          @from ||= data["from"]["email"]
+        end
+
+        def to
+          @to ||= data["personalizations"]
+            .select{|h| h.key? "to" }
+            .map{|(h)| h["to"]["email"] }
+            .first
+
+        end
+
+        def subject
+          @subject ||= data["subject"]
+        end
+
+      end # class Email
+    end # class Fakesendgrid
+  end # class Tester
+end # module Webspicy
